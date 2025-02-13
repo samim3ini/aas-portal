39c3a5425597ccad86b5a87657ef2e05010fa286
samim3ini <sjm111cia@gmail.com>
2025-02-13 07:25:37 +0300

    typo

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Authenticator } from '@aws-amplify/ui-react';
-import App from "./App.js";
+import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
