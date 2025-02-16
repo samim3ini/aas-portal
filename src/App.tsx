57ca9df94d3d90660e5ba4263854276074be5e3d
samim3ini <sjm111cia@gmail.com>
2025-02-17 02:26:30 +0300

    // Content:

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import React from 'react';
 import { useAuthenticator } from '@aws-amplify/ui-react';
+import Dashboard from './Dashboard';
 
 function App() {
   const { signOut } = useAuthenticator();
 
   return (
     <main>
+      <Dashboard />
       <button onClick={signOut}>Sign out</button>
     </main>
   );
